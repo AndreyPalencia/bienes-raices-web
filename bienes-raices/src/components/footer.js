de90488cfe859280a8133f0fa55151cdf48f174a
Andrey Palencia <ajedrezandrey@gmail.com>
2024-09-17 20:37:28 -0500

    Componente del footer de la pagagina

---
+++ bienes-raices/src/components/footer.js
@@ -0,0 +1,16 @@
+function footer() {
+    return (
+        <footer className="footer seccion">
+            <div className="contenedor contenedor-footer">
+                <nav className="navegacion">
+                    <a href="">Nosotros</a>
+                    <a href="">Anuncios</a>
+                    <a href="">Contacto</a>
+                </nav>
+            </div>
+            <p class="copyright">Todos los derechos Reservados 2024 &copy;</p>
+        </footer>
+    )
+}
+
+export default footer;

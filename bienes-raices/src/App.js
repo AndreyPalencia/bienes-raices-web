3e31a449922a898070456202a7b7fd52e42d3a5a
lmejiamartinez <lmejiamartinez25@gmail.com>
2024-09-19 18:37:27 -0500

    commit merg

--- bienes-raices/src/App.js
+++ bienes-raices/src/App.js
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Anunucios from './pages/anuncios';
 import Contacto from './pages/contacto';
 import Inicio from './pages/inicio';
+import Login from './pages/login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/nosotros" element={<Nosotros/>}></Route>
         <Route path="/anuncios" element={<Anunucios/>}></Route>
         <Route path="/contacto" element={<Contacto/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
       </Routes>
     </Router>
   );

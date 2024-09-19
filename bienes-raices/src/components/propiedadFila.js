7d53e4798a0489aafb98d3763d23e0c4b8379320
Andrey Palencia <ajedrezandrey@gmail.com>
2024-09-19 17:21:05 -0500

    Modificacion de class a className en footer y propiedadFila agragaicon del metodo de eliminacion

--- bienes-raices/src/components/propiedadFila.js
+++ bienes-raices/src/components/propiedadFila.js
@@ -1,16 +1,28 @@
-function propiedadFila({ casa }) {
+import axios from "axios";
 
+function propiedadFila({ casa }) {
+    const rutaImg = require("../assets/imagenes/" + casa.imagen);
+
+    const handleDelete = async (e) => {
+        e.preventDefault();
+        try{
+            const res = await axios.delete(`http://localhost:3000/casa/admin/delete/${e.target.value}`);
+            console.log(res.data)
+        }catch(err){
+            console.log(err);
+        }
+    }
     return (
         <tr key={casa.id}>
             <td>{casa.id}</td>
             <td>{casa.titulo}</td>
             <td>{
-                <img src={'../assets/imagenes/' + casa.imagen} alt={casa.titulo} className="imagen-tabla" />
+                <img src={rutaImg} alt={casa.titulo} className="imagen-tabla" />
             }</td>
             <td>{casa.precio}</td>
             <td>
                 <a href="" className="boton-amarillo-block">Actualizar</a>
-                <input type="submit" className="boton-rojo-block" value="Eliminar" />
+                <button value={casa.id} className="boton-rojo-block" onClick={handleDelete}>Eliminar</button>
             </td>
         </tr>
     )

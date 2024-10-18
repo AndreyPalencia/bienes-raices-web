4571fafe6a56473dabbc71e685ce54d0fb71754e
Johan Andrey Palencia Ramirez <112025660+AndreyPalencia@users.noreply.github.com>
2024-10-18 10:17:08 -0500

    Update db.js
    Quitar contraseña

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'AnDrEy200509',
+    password: 'Addd',
     database: 'bienesraices_crud',
     waitForConnections: true,
     connectionLimit: 10,
